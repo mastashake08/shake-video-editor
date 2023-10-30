c1d015f08c7048bfe5c38001676902c808e72b2c
Hacker God <jyrone.parker@gmail.com>
2023-10-30 01:22:58 -0400

    Recorder work

--- classes/transformers/RecorderTransformer.js
+++ classes/transformers/RecorderTransformer.js
@@ -1,25 +1,40 @@
 class RecorderTransformer extends TransformStream {
     encodedChunks = []
-    file = null
+    fileHandler = null
+    writableStream = null
     constructor({
-        transformer = {
-            transform(chunk, controller) {
-                const buff = new ArrayBuffer(chunk.byteLength)
-                chunk.copyTo(buff)
-                this.encodedChunks.push(buff)
-                controller.enqueue(chunk)
-                
-            },
-            flush(controller) {
-                this.file = new
-            }
-        }, 
+        transformer = this.setTransformer(), 
         writableStrategy = null, 
         readableStrategy = null,
         fileHandler = null
         }) {
             super(transformer, writableStrategy, readableStrategy)
             this.fileHandler = fileHandler
+            this.setWritable()
+            
+    }
+
+
+    async setWritable() {
+        this.writableStream = await this.fileHandler.createWritable()
+
+    }
+
+    setTransformer() {
+        return {
+            async transform(chunk, controller) {
+                const buff = new ArrayBuffer(chunk.byteLength)
+                chunk.copyTo(buff)
+                this.encodedChunks.push(chunk)
+                this.writableStream.write(buff)
+                controller.enqueue(chunk)
+                
+            },
+            async flush(controller) {
+            
+                await this.writableStream.close();
+            }
+        }
     }
 }
 
